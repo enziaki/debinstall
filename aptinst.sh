8cb07aac7d86bb8579c9b986d4d087d88039c37e
Einzeloner <58030340+Einzeloner@users.noreply.github.com>
2020-05-31 18:12:56 +0530

    Update aptinst.sh

--- aptinst.sh
+++ aptinst.sh
@@ -1,2 +1,2 @@
 sudo apt-get update && sudo apt upgrade -y
-sudo apt install wine steam gimp lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch q4wine xorriso vlc torbrowser-launcher spotify-client youtube-dl audacity handbrake -y
+sudo apt install wine steam gimp lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch q4wine xorriso vlc torbrowser-launcher spotify-client youtube-dl audacity handbrake psensor -y
