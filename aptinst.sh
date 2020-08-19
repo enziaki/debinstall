602d321fa3d3bc92330472f97a5b2d9c6ee55135
Einzeloner <58030340+Einzeloner@users.noreply.github.com>
2020-08-19 06:31:14 +0530

    Update aptinst.sh

--- aptinst.sh
+++ aptinst.sh
@@ -1,3 +1,3 @@
 sudo apt-get update && sudo apt upgrade -y
-sudo apt install wine steam gimp lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch q4wine xorriso vlc torbrowser-launcher spotify-client youtube-dl audacity handbrake psensor gnome-shell-extension-pixelsaver gnome-shell-extension-dash-to-panel -y
+sudo apt install wine steam gimp lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch q4wine xorriso vlc torbrowser-launcher spotify-client youtube-dl audacity handbrake psensor gnome-shell-extension-pixelsaver gnome-shell-extension-dash-to-panel obs-studio -y
 
