72299fb3cd0832695a1a248953d5eed2510f4652
enziaki <58030340+enziaki@users.noreply.github.com>
2021-08-16 13:07:31 +0530

    added vim-gtk3

--- aptinst.sh
+++ aptinst.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && sudo apt upgrade -y
-sudo apt install wine steam gimp lolcat zsh figlet discord htop bashtop lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch bashtop q4wine xorriso vlc torbrowser-launcher youtube-dl audacity handbrake psensor gnome-shell-extension-pixelsaver gnome-shell-extension-dash-to-panel obs-studio -y
+sudo apt install wine steam gimp lolcat zsh figlet vim-gtk3 discord htop bashtop lutris gnome-tweak-tool gnome-shell-extensions tlp powertop neofetch bashtop q4wine xorriso vlc torbrowser-launcher youtube-dl audacity handbrake psensor gnome-shell-extension-pixelsaver gnome-shell-extension-dash-to-panel obs-studio -y
 #Spawn commands
 neofetch
-figlet Honh Honh Baguette Willy san nyaaa~ | lolcat
+figlet UwU~ | lolcat
